b0f1a138d2676cbbab84a9a52ec3eaf3957c02e5
Hundd <hundd@rambler.ru>
2017-08-05 13:39:28 +0300

    First Practice

---
+++ practice1.js
@@ -0,0 +1,88 @@
+/*1.9. Даны два числа. Найти среднее арифметическое и среднее геометрическое их
+модулей.
+ */
+function findAverage(a, b) {
+
+    var result = {};
+
+    result.arithmetic = (Math.abs(a) + Math.abs(b)) / 2
+    result.geometric = Math.sqrt(Math.abs(a) * Math.abs(b));
+
+    return result;
+}
+
+
+
+/*2.9. Дано двузначное число. Определить:
+а) входят ли в него цифры 4 или 7;
+б) входят ли в него цифры 3, 6 или 9.
+*/
+function findSomeDigits(digit) {
+    var result = {};
+
+    result.itHas4or7 = /[47]/.test(digit);
+    result.itHas3or6or9 = /[369]/.test(digit);
+
+    return result;
+}
+
+
+
+/*3.9. Два отрезка на плоскости заданы целочисленными координатами своих концов в декартовой системе координат.
+ Требуется определить, существует ли у них общая точка. */
+function crossLines(x1, y1, x2, y2, x3, y3, x4, y4) {
+    //Find out minimum and maximum values for each point;
+    var p1XMin = x1 < x2 ? x1 : x2,
+        p1XMax = x1 > x2 ? x1 : x2,
+        p1YMin = y1 < y2 ? y1 : y2,
+        p1YMax = y1 > y2 ? y1 : y2,
+
+        p2XMin = x3 < x4 ? x3 : x4,
+        p2XMax = x3 > x4 ? x3 : x4,
+        p2YMin = y3 < y4 ? y3 : y4,
+        p2YMax = y3 > y4 ? y3 : y4;
+
+    if ((p1XMin <= p2XMax && p2XMin <= p1XMax && p1YMin <= p2YMax && p2YMin <= p1YMax) === false) {
+
+        //Two sections can't have commond dot because they're lying in different spaces;
+        return false;
+    }
+
+    var dx1 = x2 - x1;
+    var dy1 = y2 - y1;
+    var dx2 = x4 - x3;
+    var dy2 = y4 - y3;
+
+    //canonical solution for line y = a x + b;
+    var a1 = dy1 / dx1,
+        a2 = dy2 / dx2;
+
+    if (a1 === a2) {
+        //Lines are parallel
+        return false;
+    }
+    if (isFinite(a1) && isFinite(a2)) {
+        //Lines can be described with a canonical solution y = a x + b;
+        //Find a common point
+        //y = a1 x + b1;
+        //y = a2 x + b2;
+
+        var b1 = y1 - a1 * x1,
+            b2 = y3 - a2 * x3;
+
+        var x = (b2 - b1) / (a1 - a2),
+            y = a1 * x + b1;
+
+        return (
+            x <= p1XMax && x <= p2XMax &&
+            x >= p1XMin && x >= p2XMin &&
+            y <= p1YMax && y <= p2YMax &&
+            y >= p1YMin && y >= p2YMin
+        );
+
+    }
+    // if lines are crossing and perpendicular;
+    return true;
+}
+
+module.exports = { findAverage, findSomeDigits, crossLines };
---
+++ spec/practice1.spec.js
@@ -0,0 +1,77 @@
+var findAverage = require("../practice1").findAverage,
+    findSomeDigits = require("../practice1").findSomeDigits,
+    crossLines = require("../practice1").crossLines;
+
+describe("Find Average: ", function() {
+
+    it("Average 0, 0", function() {
+        var result = findAverage(0, 0);
+        expect(result.arithmetic).toBe(0);
+        expect(result.geometric).toBe(0);
+    });
+
+    it("Average 4, 9", function() {
+        var result = findAverage(4, 9);
+        expect(result.arithmetic).toBe(6.5);
+        expect(result.geometric).toBe(6);
+    });
+
+});
+
+describe("Find Some Digits in the number: ", function() {
+
+    it("has not 4 or 7", function() {
+        expect(findSomeDigits(1235689).itHas4or7).toBe(false);
+    });
+
+    it("has 4 or 7", function() {
+        expect(findSomeDigits(45689).itHas4or7).toBe(true);
+    });
+
+    it("has neither 3 nor 6 nor 9", function() {
+        expect(findSomeDigits(124578).itHas3or6or9).toBe(false);
+    });
+
+    it("has 3 or 6 or 7", function() {
+        expect(findSomeDigits(123).itHas3or6or9).toBe(true);
+    });
+
+    it("has 3 and 4", function() {
+        expect(findSomeDigits(1234).itHas4or7 && findSomeDigits(1234).itHas3or6or9).toBe(true);
+    });
+
+});
+
+describe("Cross lines: ", function() {
+    it("Regular crossing lines", function() {
+        expect(crossLines(0, 0, 5, 5, 0, 5, 5, 0)).toBe(true);
+    });
+
+    it("Regular lines are not crossing", function() {
+        expect(crossLines(1, 1, 4, 4, 1, 2, 2, 5)).toBe(false);
+    });
+
+    it("Parralel lines", function() {
+        expect(crossLines(0, 0, 5, 5, 1, 1, 6, 6)).toBe(false);
+    });
+
+    it("Horisontal and Vertical lines are crossing", function() {
+        expect(crossLines(0, 5, 5, 5, 2, 2, 2, 8)).toBe(true);
+    });
+
+    it("Horisontal and Vertical lines are not crossing", function() {
+        expect(crossLines(0, 0, 6, 0, 1, 1, 1, 3)).toBe(false);
+    });
+
+    it("Vertical line with regular line are crossing", function() {
+        expect(crossLines(0, 0, 0, 6, -1, 1, 3, 3)).toBe(true);
+    });
+
+    it("Vertical line with regular line are not crossing", function() {
+        expect(crossLines(0, 0, 0, 6, 1, 1, 3, 3)).toBe(false);
+    });
+
+    it("Horisontal line and regular line are crossing", function() {
+        expect(crossLines(0, 0, 6, 0, 0, -2, 6, 2)).toBe(true);
+    });
+});
